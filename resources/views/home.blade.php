77083280c002e39a2499ebe0fe36e6151fd530da
Daniel Tran <danieltran354@gmail.com>
2025-10-22 11:24:32 +0200

    Small change to front/home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
     <h1>Welcome to the Home Page</h1>
+
+    @auth
+        <p>You are logged in as <strong>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</strong></p>
+    @endauth
+
+    @guest
+        <p>You are not logged in. <a href="{{ route('login') }}">Login here</a>.</p>
+    @endguest
+
 </x-layout>
+
