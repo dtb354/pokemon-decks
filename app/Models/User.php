614f2d8e0c65628fec1dd6475b707a41c372ab13
Daniel Tran <danieltran354@gmail.com>
2025-11-02 13:31:46 +0100

    Removed an unwanted method is User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    public mixed $is_admin;
-
     /**
      * The attributes that are mass assignable.
      *
